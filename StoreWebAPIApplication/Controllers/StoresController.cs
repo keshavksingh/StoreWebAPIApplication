3db831bed2b6af9ef10ec3297d429446b4238d6d
Keshav Singh <41631964+keshavksingh@users.noreply.github.com>
2023-12-04 12:42:33 -0800

    Add project files.

---
+++ StoreWebAPIApplication/Controllers/StoresController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreWebAPIApplication.CustomActionFilters;
+using StoreWebAPIApplication.Data;
+using StoreWebAPIApplication.DataTransferObjects;
+using StoreWebAPIApplication.DomainModels;
+using StoreWebAPIApplication.Repositories;
+using System.Runtime.CompilerServices;
+
+namespace StoreWebAPIApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoresController:ControllerBase
+    {
+        private readonly ProductStoreDbContext dbProductContext;
+        private readonly IProductRepository productRepository;
+        private readonly IMapper mapper;
+
+        public StoresController(ProductStoreDbContext dbProductContext, IProductRepository productRepository, IMapper mapper)
+        {
+            this.dbProductContext = dbProductContext;
+            this.productRepository = productRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductStore([FromQuery] string? filterOn, [FromQuery] string?filterQuery)
+        {
+            //Get Data From Domain Model
+            var StoreDomain = await productRepository.GetAllProductStoreAsync(filterOn,filterQuery);
+            var ProductsDomain = await productRepository.GetAllAsync();
+            var query = from store in StoreDomain
+                        join product in ProductsDomain on store.ProductId equals product.ProductId
+                        select new ProductStoreDto
+                        {
+                            StoreName = store.StoreName,
+                            ProductName = product.ProductName,
+                            ProductPrice = product.ProductPrice
+                        };
+            //Convert Domain Model and Map to DTOs
+            //var ProductsDto = new List<ProductDto>();
+            //foreach (var product in ProductsDomain) 
+            //{ 
+            //    ProductsDto.Add(new ProductDto 
+            //    {  ProductId = product.ProductId,
+            //        ProductName=product.ProductName,
+            //        ProductPrice=product.ProductPrice,
+            //        DateModified = product.DateModified
+            //    });
+            //}
+            //var ProductsDto = mapper.Map<List<ProductDto>>(ProductsDomain);
+            return Ok(query.ToList());
+
+        }
+    }
+}
---
+++ StoreWebAPIApplication/Migrations/20231130224635_Initial Migration.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreWebAPIApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    StoreId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StoreName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.StoreId);
+                    table.ForeignKey(
+                        name: "FK_Stores_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stores_ProductId",
+                table: "Stores",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stores");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
